89b400c27d8de5d890e046f6a06d8c5adf55587d
yogesh k <63460812+yokesh492@users.noreply.github.com>
2024-02-05 23:17:24 +0530

    Update models.py

--- app/api/models.py
+++ app/api/models.py
@@ -1,25 +1,25 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+# from sqlalchemy import Column, Integer, String, Float, ForeignKey
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import relationship
 
-Base = declarative_base()
+# Base = declarative_base()
 
-class User(Base):
-    __tablename__ = "users"
+# class User(Base):
+#     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    password = Column(String)
-    catalogue = relationship("Catalogue", back_populates="owner")
+#     id = Column(Integer, primary_key=True, index=True)
+#     username = Column(String, unique=True, index=True)
+#     password = Column(String)
+#     catalogue = relationship("Catalogue", back_populates="owner")
     
-class Catalogue(Base):
-    __tablename__ = "catalogue"
+# class Catalogue(Base):
+#     __tablename__ = "catalogue"
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    description = Column(String)
-    price = Column(Float)
-    qty = Column(Integer)
-    image = Column(String)
-    user_id = Column(Integer, ForeignKey("users.id"))
-    owner = relationship("User", back_populates="catalogue")
+#     id = Column(Integer, primary_key=True, index=True)
+#     name = Column(String)
+#     description = Column(String)
+#     price = Column(Float)
+#     qty = Column(Integer)
+#     image = Column(String)
+#     user_id = Column(Integer, ForeignKey("users.id"))
+#     owner = relationship("User", back_populates="catalogue")
