4d8c265e699016bfa9137cf31a22f3e3754f617e
yogesh k <63460812+yokesh492@users.noreply.github.com>
2024-02-06 22:25:11 +0530

    Update routes.py

--- app/api/routes.py
+++ app/api/routes.py
@@ -4,6 +4,8 @@ from sqlalchemy.orm import Session
 # from app.api import auth, crud, deps, models
 from app.api.utils import process_image, get_gemini_response
 from fastapi.responses import JSONResponse
+import logging
+logger = logging.getLogger(__name__)
 
 router = APIRouter()
 
@@ -34,12 +36,19 @@ async def process_image_endpoint(
     uploaded_file: UploadFile = File(...)
 ):
     try:
+        logger.info(f"Uploaded file name: {uploaded_file.filename}")
+        logger.info(f"Uploaded file content type: {uploaded_file.content_type}")
+        logger.info(f"Uploaded file size: {uploaded_file.file.__sizeof__()}")
         image_content = process_image(uploaded_file)
+        logger.info(f"Processed image content: {image_content}")
+
         image_data = [{"mime_type": uploaded_file.content_type, "data": image_content}]
         #print(image_data)
         gemini_response = get_gemini_response(image_data)
         #print(gemini_response)
         return JSONResponse(content=gemini_response)#content={"response": gemini_response}
     except FileNotFoundError as e:
+        logger.exception("FileNotFoundError occurred while processing the image")
         raise HTTPException(status_code=404, detail=str(e))
+    
     
