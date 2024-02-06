72424888978e7f33cee8dd7d18130f3d0ade2b4e
yogesh k <63460812+yokesh492@users.noreply.github.com>
2024-02-06 22:05:33 +0530

    Update main.py cros functionality

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from app.api.routes import router as api_router
 from app.api.auth import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router)
 app.include_router(router)
