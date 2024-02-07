ba7fdd5ce4b95e5dd35fc64b7dd5cc50c5fda43c
yogesh k <63460812+yokesh492@users.noreply.github.com>
2024-02-07 22:26:28 +0530

    Update utils.py

--- app/api/utils.py
+++ app/api/utils.py
@@ -2,23 +2,28 @@ from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.responses import JSONResponse
 from PIL import Image
 from io import BytesIO
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
+from google.cloud import storage
 import os
 import google.generativeai as genai
 import json
 import re
 
-# load_dotenv()
+load_dotenv()
 genai.configure(api_key="AIzaSyDo3bbDydm0fN9V2es__wTP_QAD7nwDXO0")
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r"secret_key\ONDC_GCP.json"
+client = storage.Client()
+bucket_name = 'ondc_hackathonimage'
+
+
 model = genai.GenerativeModel('gemini-pro-vision')
 model2 = genai.GenerativeModel('gemini-pro')
-
 prompt = """
     you are an expert ecommerce seller who is about to digitalize the catalogue , 
     from the given image extract the data such as 
     {
     name: name of the product,
-    description: description of the product,
+    description: write the description for less than 30 words of the product dont leave empty,
     price: price of the product if its mention in image else give leave empty,
     category: top 3 category from this   
     - Diary Products 
@@ -44,7 +49,7 @@ prompt = """
 """
 
 
-def get_gemini_response(image_data):
+async def get_gemini_response(image_data):
 
     response = model.generate_content([ image_data[0], prompt])
     #response = json.dump(response.text)
@@ -59,25 +64,35 @@ def get_gemini_response(image_data):
 def process_image(uploaded_file: UploadFile):
     if uploaded_file.content_type not in ["image/jpeg", "image/png"]:
         raise HTTPException(status_code=400, detail="Unsupported image format")
+    uploaded_file.file.seek(0)
     image_content = uploaded_file.file.read()
     #print("################################################################",image_content)
     if not image_content:
         raise HTTPException(status_code=400, detail="Empty image content")
 
     #image = Image.open(BytesIO(uploaded_file.file.read()))
-    return image_content
-def get_gemini_text(input_text):
+    return  image_content
+
+
+async def get_gemini_text(input_text):
     prompt = f"""
     you are an expert ecommerce seller who is about to digitalize the catalogue , 
     from the given product summary 
-    {input_text}  extract the data such as name , description , category , price if mention ina above ,varients such as color ,size if it mention
-    and  if product summary does not contain enough information to extract the requested data fields. just give output no issue, domt give null else give empty string
+    {input_text}  extract the data such as name , description based on retrive product name , category , price if mention in summary if null then empty string ,varients such as color ,size if it mention.
+    if product summary does not contain enough information to extract the requested data fields. just give output , give empty string.
     give output in this json format.
      """
     response = model2.generate_content(prompt)
-    #print(response.text)
+    print(response.text)
     text = response.text.replace('json','')
-    #print(text)
+    print(text)
     text = eval(text.replace('`',''))
     return text
 
+async def upload_image_to_gcs(uploaded_file: UploadFile):
+    bucket = client.bucket(bucket_name)
+    blob = bucket.blob(uploaded_file.filename)
+    blob.upload_from_file(uploaded_file.file)
+    image_url = f'https://storage.googleapis.com/{bucket_name}/{blob.name}'
+    image_url = blob.public_url
+    return  image_url
