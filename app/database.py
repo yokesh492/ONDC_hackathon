fe2df567fc4c11deeaa215f90c0bf4044ae2b053
yogesh k <63460812+yokesh492@users.noreply.github.com>
2024-02-07 22:31:50 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,14 +1,27 @@
-# import databases
-# import sqlalchemy
-# from sqlalchemy.orm import sessionmaker
+import databases
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
 
-# DATABASE_URL = "postgresql://postgres:Pokemon492#@localhost/Product"
+#DATABASE_URL = "postgresql://postgres:Pokemon492#@localhost/Product" #for testing local
+DATABASE_URL = "postgresql+psycopg2://yoke492:Pokemon492#@/Product?host=/cloudsql/total-method-413610:asia-south2:ondc-hackathon"
+database = databases.Database(DATABASE_URL)
+metadata = sqlalchemy.MetaData()
 
-# database = databases.Database(DATABASE_URL)
-# metadata = sqlalchemy.MetaData()
+engine = sqlalchemy.create_engine(
+    DATABASE_URL
+)
+metadata.create_all(engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# engine = sqlalchemy.create_engine(
-#     DATABASE_URL
-# )
-# metadata.create_all(engine)
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+# testing conneciton
+# engine = sqlalchemy.create_engine(DATABASE_URL)
+
+
+# try:
+#     connection = engine.connect()
+#     print("Connection to the database successful!")
+#     connection.close()
+# except Exception as e:
+#     print(f"Error connecting to the database: {e}")
